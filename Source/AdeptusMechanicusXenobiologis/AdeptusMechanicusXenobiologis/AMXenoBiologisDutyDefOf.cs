886efb530d52162b0a28e345b1580bc88ebe1599
Ogliss <randomdragon2@hotmail.com>
2020-02-17 17:57:47 +0000

    V1.5 Inital Commit

---
+++ Source/AdeptusMechanicusXenobiologis/AdeptusMechanicusXenobiologis/AMXenoBiologisDutyDefOf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Verse.AI;
+
+namespace RimWorld
+{
+    [DefOf]
+    public static class AMXenoBiologisDutyDefOf
+    {
+        public static DutyDef OGAMXB_DefendAndExpandHiveLike;
+        public static DutyDef OGAMXB_DefendHiveLikeAggressively;
+        public static DutyDef OGAMXB_AssaultColony;
+    }
+}
---
+++ Source/AdeptusMechanicusXenobiologis/AdeptusMechanicusXenobiologis/Racial/Orkz/OGOrkDefOf.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Verse;
+
+namespace RimWorld
+{
+    // Token: 0x02000956 RID: 2390
+    [DefOf]
+    public static class OGOrkDefOf
+    {
+        // Token: 0x06003781 RID: 14209 RVA: 0x001A8393 File Offset: 0x001A6793
+        static OGOrkDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(OGOrkDefOf));
+        }
+
+        // Ork ThingDefs
+        public static ThingDef Alien_Ork;
+        public static ThingDef Alien_Grot;
+        public static ThingDef OG_Alien_Ork;
+        public static ThingDef OG_Alien_Grot;
+
+        // Ork FactionDefs
+        public static FactionDef OrkFaction;
+        public static FactionDef FeralOrkFaction;
+        public static FactionDef HulkOrkz;
+        public static FactionDef RokOrkz;
+
+        public static FactionDef OG_Ork_Tek_Faction;
+        public static FactionDef OG_Ork_Feral_Faction;
+        public static FactionDef OG_Ork_Hulk;
+        public static FactionDef OG_Ork_Rok;
+
+        // Ork IncidentDefs
+        public static IncidentDef OG_Ork_Rok_Crash;
+
+        // Ork DamageDefs
+        //    public static DamageDef ;
+
+        // Ork HediffDefs
+        //    public static HediffDef ;
+
+        // Ork MentalStateDefs
+        //    public static MentalStateDef ;
+
+        // Ork GameConditionDefs
+        //    public static GameConditionDef ;
+
+        // Ork PawnKindDefs
+        //    public static PawnKindDef ;
+    }
+}
